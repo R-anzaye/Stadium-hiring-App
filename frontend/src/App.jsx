7fce09dfd622eca8a3136caed7a2306cfae9b6c0
Ryan Anzaye <rileydollar8@gmail.com>
2024-07-13 00:28:43 +0300

    netlify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { UserProvider } from './UserContext';
 import { TaskProvider } from './TaskContext';
@@ -9,37 +9,44 @@ import BookPitch from './BookPitch';
 import ReviewForm from './ReviewForm';
 import Admin from './admin';
 import ProtectedRoute from './ProtectedRoute';
-import { AdminProvider } from './AdminContext'
+import { AdminProvider } from './AdminContext';
 import Pitches from './Pitches';
 import AdminBookings from './AdminBookings';
 
 function App() {
+  useEffect(() => {
+    // Redirect to homepage if not already on it
+    if (!window.location.pathname.startsWith('/')) {
+      window.location.href = '/';
+    }
+  }, []);
+
   return (
     <Router>
       <UserProvider>
         <AdminProvider>
-        <TaskProvider>
-          <ReviewsProvider>
-            <AppProvider>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/bookpitch" element={<BookPitch />} />
-                <Route path="/reviews" element={<ReviewForm />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/pitches" element={<Pitches />} />
-                <Route path="/bookings" element={<AdminBookings />} />
-                <Route
-                  path="/admin"
-                  element={
-                    <ProtectedRoute admin>
-                      <Admin />
-                    </ProtectedRoute>
-                  }
-                />
-              </Routes>
-            </AppProvider>
-          </ReviewsProvider>
-        </TaskProvider>
+          <TaskProvider>
+            <ReviewsProvider>
+              <AppProvider>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/bookpitch" element={<BookPitch />} />
+                  <Route path="/reviews" element={<ReviewForm />} />
+                  <Route path="/admin" element={<Admin />} />
+                  <Route path="/pitches" element={<Pitches />} />
+                  <Route path="/bookings" element={<AdminBookings />} />
+                  <Route
+                    path="/admin"
+                    element={
+                      <ProtectedRoute admin>
+                        <Admin />
+                      </ProtectedRoute>
+                    }
+                  />
+                </Routes>
+              </AppProvider>
+            </ReviewsProvider>
+          </TaskProvider>
         </AdminProvider>
       </UserProvider>
     </Router>
